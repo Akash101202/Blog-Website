f2a39a0a262625f761383327cbff1a105a723798
P.Akash <p.akash101202@gmail.com>
2024-07-05 22:51:32 +0530

    Final Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
     const fetchBlogs = async () => {
       try {
         const { data } = await axios.get(
-          axioms.get(`${backendUrl}/api/v1/blog/all`),
+          axios.get(`${backendUrl}/api/v1/blog/all`),
           { withCredentials: true }
         );
         setBlogs(data.allBlogs);
