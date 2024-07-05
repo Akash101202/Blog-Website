ea40d9582d4ba659957e0863a65c0c8f05db6e60
P.Akash <p.akash101202@gmail.com>
2024-07-05 22:23:55 +0530

    'Changes'

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'https://blog-website-three-lake.vercel.app', // Update this to match your frontend URL
+  origin: 'https://blog-website-bay-six.vercel.app', // Update this to match your frontend URL
   credentials: true,
 };
 
